6aab7d80456045942bf92af5e4dd778b497bfe97
MikeWazowsky <58822373+GabrielMerchan@users.noreply.github.com>
2026-01-21 00:11:44 +0100

    Primera subida
    Lo que llevo hasta ahora del cursus

---
+++ Cursus/ft_libc/ft_memchr.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamercha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 20:50:38 by gamercha          #+#    #+#             */
+/*   Updated: 2026/01/20 20:50:42 by gamercha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *str;
+    unsigned char chr;
+    size_t i;
+
+    if(!s||c<0)
+        return NULL;
+    i=0;
+    chr = (unsigned char)c;
+    str= (const unsigned char*)s;
+    while(n--)
+    {
+        if(*str==chr)
+            return ((void *)str);
+        str++;
+    }
+    if(*str=='\0')
+        return ((void *)str);
+    return NULL;
+}
+//  void printArray(int arr[], int n)
+// {
+//    for (int i=0; i<n; i++)
+//       printf("%d ", arr[i]);
+// }
+
+// int main ()
+// {
+//     // char *s="ABCDEFGHIJKLMN";
+//     // char c= 69;
+//     int c = 2;
+//     int s[4]={1,2,3,4};
+//     int *f;
+//     int *g;
+//     // char *f;
+//     // char *g;
+//     //printf("Antes: %s\n", s);
+//     f=ft_memchr(s, c, 4*(sizeof(int)));
+//     //printf("Despues: %s", f);
+//     printArray(f,4);
+//     printf("\n");
+//     g=memchr(s, c, 4*(sizeof(int)));
+//     //printf("\nOficial: %s", g);
+//     printArray(g,4);
+//     printf("\n");
+//     return 0;
+// }
---
+++ Cursus/ft_libc/ft_strncmp.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamercha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/18 17:57:50 by gamercha          #+#    #+#             */
+/*   Updated: 2026/01/20 19:57:03 by gamercha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+// #include <stdio.h>
+#include <string.h>
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+    unsigned char *str1;
+    unsigned char *str2;
+
+    if(!s1||!s2)
+        return (0);
+    str1= (unsigned char*)s1;
+    str2= (unsigned char*)s2;
+    i=0;
+
+    while (str1[i]&&str2[i]&& i<n-1)
+    {
+        if(str1[i]==str2[i])
+            i++;
+        else
+            return str1[i]-str2[i];
+    }
+    return (str1[i]-str2[i]);
+}
+
+// int main (int argc, char *argv[])
+// {
+//     int n=0;
+//     if(argc!= 3)
+//         return -1;
+//     n=ft_strncmp(argv[1], argv[2], 5);
+//     printf("Propia:\ns1: %s\ns2: %s\nn: %d\n", argv[1], argv[2], n);
+//     n=strncmp(argv[1], argv[2], 5);
+//     printf("Oficial:\ns1: %s\ns2: %s\nn: %d\n", argv[1], argv[2], n);
+//     return 0;
+// }
