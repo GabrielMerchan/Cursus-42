cc0c2349bfd8341bd65ebb3a88b747e492bc4b95
GabrielMerchan <gabriel.merchan.romero@gmail.com>
2026-01-27 01:53:59 +0100

    trim terminado

--- Cursus/ft_libc/ft_strtrim.c
+++ Cursus/ft_libc/ft_strtrim.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
-#include <stdio.h>
 size_t ft_strlen(const char *s)//esto quitarlo cuando meta la libreria
 {
     size_t i = 0;
@@ -20,32 +19,43 @@ size_t ft_strlen(const char *s)//esto quitarlo cuando meta la libreria
 }
 char    *ft_strchr(const char *s, int c)
 {
+    int i;
+
+    i = 0;
     if (!s)
         return (0);
-    while(*s)
+    while(s[i])
     {
-        if (*s == c)
-            return ((char*)s);
-        s++;
+        if (s[i] == c)
+            return ((char*)&s[i]);
+        i++;
     }
     if (c == '\0')
-		return ((char *)s);
+		return ((char *)&s[i]);
     return NULL;
 }
 char *ft_strtrim(char const *s1, char const *set)
 {
     char *str;
     int i;
+    int c;
     int j;
 
+    j=0;
     i = 0;
-    j = 0;
-    str = malloc(ft_strlen(s1)*sizeof(char));
-    str = (char*)s1;
-
+    c = 0;  
+    while(ft_strchr(set, s1[i]))
+        i++;
+    c = ft_strlen(s1);
+    while (ft_strchr(set, s1[c]))
+        c--;
+    str = malloc((c-i)*sizeof(char)+2);
+    while (i<=c)
+        str[j++]=s1[i++];
+    str[j]='\0';
     return str;
 }
-#include <stdio.h>
+/*#include <stdio.h>
 int main (int argc, char *argv[])
 {
     if (argc!= 3)
@@ -53,3 +63,5 @@ int main (int argc, char *argv[])
     printf("Primera: %s\nQuitar: %s\nResultado: %s\n", argv[1], argv[2], ft_strtrim(argv[1],argv[2]));
     return 0;
 }
+//Pongo +2 en el malloc porque 1 es por el nulo que no pilla 
+//el strlen de c y otro para el nulo propio de str*/
